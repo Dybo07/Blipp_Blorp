4eb266859751318ffc1f0588b704abccdc2adfec
Dybo07 <159918948+Dybo07@users.noreply.github.com>
2026-08-21 15:05:46 +0200

    Working movement and jump + camera tracking
    Basic movement
    Left and right
    jump
    Camera tracking
    and flipping character depending on the movement direction

--- Assets/Dylan Temp/PlayerMovement.cs
+++ Assets/Dylan Temp/PlayerMovement.cs
@@ -3,26 +3,74 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    public float moveInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Camera")]
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Vector2 cameraOffset;
+    [SerializeField] private float cameraSmoothSpeed = 5f;
+    [SerializeField] private float smoothSpeed = 5f;
+
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    public GameObject spritePlayer;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+    private bool isGrounded;
+
     void Start()
     {
+        playerCamera = Camera.main;
         rb = GetComponent<Rigidbody2D>();
-
     }
-    public void OnMove(InputAction.CallbackContext context)
+
+    void Update()
     {
-        moveInput = context.ReadValue<float>();
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        if (moveInput > 0)
+            spritePlayer.GetComponent<SpriteRenderer>().flipX = false;
+        else if (moveInput < 0)
+            spritePlayer.GetComponent<SpriteRenderer>().flipX = true;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+    private void LateUpdate()
+    {
+        if (playerCamera == null) return;
+
+        Vector3 targetPosition = new Vector3(
+            transform.position.x + cameraOffset.x,
+            transform.position.y + cameraOffset.y,
+            playerCamera.transform.position.z
+        );
+
+        playerCamera.transform.position = Vector3.Lerp(
+            playerCamera.transform.position,
+            targetPosition,
+            smoothSpeed * Time.deltaTime
+        );
+
 
     }
-  
 }
