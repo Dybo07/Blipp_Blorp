aae9f27c97cf2c8ed3a94554a56a51a232ac7ad5
Dybo07 <159918948+Dybo07@users.noreply.github.com>
2026-08-28 12:20:37 +0200

    UI stuff

--- Assets/Assets/Scripts/SceneManager.cs
+++ Assets/Assets/Scripts/SceneManager.cs
@@ -6,15 +6,19 @@ public class SceneManager : MonoBehaviour
 
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+
         
+    }
+
+    void play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMap");
+    }
+    void quit()
+    {
+        Application.Quit();
     }
 }
